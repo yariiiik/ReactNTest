199dd693823c4ed4a25e67d24defa5301d3ad6ca
yariiiik <vmene.e@i.ua>
2024-05-13 13:41:28 +0200

    переделка массива компонентов в обьект данных

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,258 +1,236 @@
 import React, { useState } from "react";
-import { View, Text, StyleSheet, SafeAreaView, Dimensions, ScrollView } from "react-native";
-
+import { View, Text, StyleSheet, SafeAreaView, Dimensions, ScrollView, FlatList } from "react-native";
 
 export default function SettingsScreen() {
-	let smeni_names = ["🍌", "🍑", "🍓", "🍒"];
-	let shifts = [0, 14, 7, 21];
-	let dninedeli = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+  let smeni_names = ["🍌", "🍑", "🍓", "🍒"];
+  let shifts = [0, 14, 7, 21];
+  let dninedeli = ["Пн", "Вт", "Ср", "Чт", "Пт", "Сб", "Вс"];
+  let graf_shifts = ["N", "N", "N", "N", "N", "N", "🍺", "O", "O", "O", "O", "🍺", "R", "R", "R", "🍺", "🍺", "🍺", "O", "O", "O", "🍺", "R", "R", "R", "R", "🍺", "☕️"];
 
-	// const screenHeight = Dimensions.get('window').height;
-	// console.log("🚀 ~ SettingsScreen ~ screenHeight:", screenHeight);
+  const [savedMonth, setSavedMonth] = useState(0);
 
-	let graf_shifts = ["N", "N", "N", "N", "N", "N", "🍺", "O", "O", "O", "O", "🍺", "R", "R", "R", "🍺", "🍺", "🍺", "O", "O", "O", "🍺", "R", "R", "R", "R", "🍺", "☕️"];
-	// let graf_shifts = ["N", "N", "N", "N", "N", "N", " ", "O", "O", "O", "O", " ", "R", "R", "R", " ", " ", " ", "O", "O", "O", " ", "R", "R", "R", "R", " ", " "];
+  let getUserDate = () => {
+    let UD = new Date();
+    UD.setMonth(UD.getMonth() + savedMonth);
+    let UDFullYear = UD.getFullYear();
+    let UDMonth = UD.getMonth() + 1;
 
+    let UDStart = new Date(`${UDFullYear}-${UDMonth}-1`);
+    let UDLastDay = new Date(UDFullYear, UDMonth, 0);
+    let UDEnd = new Date(`${UDFullYear}-${UDMonth}-${UDLastDay.getDate()}`);
+    let UDStartDayOfYear = Math.round((UDStart - new Date(2024, 0, 1)) / (24 * 60 * 60 * 1000));
+    return { UDStart, UDEnd, UDStartDayOfYear }
+  }
 
-	const [savedMonth, setSavedMonth] = useState(0);
+  let createMassDate = (print_shift) => {
+    const today = new Date();
+    const todayDay = today.getDate();
+    const todayMonth = today.getMonth() + 1;
+    let UserDate = getUserDate();
+    let day, month, weekDay, dateText;
+    let massObjData = [];
+    while (UserDate.UDStart <= UserDate.UDEnd) {
+      let currentshifts = [];
+      day = UserDate.UDStart.getDate();
+      month = UserDate.UDEnd.getMonth() + 1;
 
-	let getUserDate = () => {
-		let today = new Date();
-		today.setMonth(today.getMonth() + savedMonth);
-		let todayFullYear = today.getFullYear();
-		let todayMonth = today.getMonth() + 1;
+      dateText = `${day < 10 ? '0' + day : day}.${month < 10 ? '0' + month : month}`;
+      weekDay = dninedeli.at(UserDate.UDStartDayOfYear % 7);
 
-		let startD = new Date(`${todayFullYear}-${todayMonth}-1`);
-		let lastDay = new Date(todayFullYear, todayMonth, 0);
+      print_shift.forEach(element => {
+        currentshifts.push(graf_shifts[(UserDate.UDStartDayOfYear + element) % graf_shifts.length])
+      });
 
-		let endD = new Date(`${todayFullYear}-${todayMonth}-${lastDay.getDate()}`);
-		let startDayOfYear = Math.round((startD - new Date(2024, 0, 1)) / (24 * 60 * 60 * 1000));
-		return { startD, endD, startDayOfYear }
-	}
+      massObjData.push({ day: dateText, weekDay, shifts: currentshifts });
 
-	let createMassDate = (print_shift) => {
-		const today = new Date();
-		const todayDay = today.getDate();
-		const todayMonth = today.getMonth();
-		let UserDate = getUserDate();
-		let day, month, currentshift, dennedeli, dateText;
-		let massDate = [];
-		while (UserDate.startD <= UserDate.endD) {
-			day = UserDate.startD.getDate();
-			month = UserDate.endD.getMonth() + 1;
+      UserDate.UDStartDayOfYear++;
+      UserDate.UDStart.setDate(UserDate.UDStart.getDate() + 1);
+    }
+    return massObjData;
+  };
 
-			dateText = `${day < 10 ? '0' + day : day}.${month < 10 ? '0' + month : month}`;
-			dennedeli = dninedeli.at(UserDate.startDayOfYear % 7);
+  const getTodayDate = () => {
+    const today = new Date();
+    const todayDay = today.getDate();
+    const todayMonth = today.getMonth() + 1;
+    return { todayDay, todayMonth };
+  }
+  const { todayDay, todayMonth } = getTodayDate();
 
-			currentshift = graf_shifts.at((UserDate.startDayOfYear + print_shift) % graf_shifts.length);
+  const DateItem = ({ item }) => {
+    return (
+      <View style={styles.row}>
+        <CellDate today={item.day === `${todayDay}.${todayMonth}`} T={item.day} />
+        <WeekDay dn={item.weekDay} />
+        {item.shifts.map((element, index) => (
+          <Yachejka el={element} key={index.toString()} />
+        ))}
+      </View>
+    );
+  };
 
+  const CellDate = ({ today, T }) => {
+    return (<Text style={today ? styles.dateToday : styles.date}>{T}</Text>)
+  };
+  const WeekDay = ({ dn }) => {
+    if (dn == "Сб" || dn == "Вс") { return (<Text style={styles.textDenNedV}>{dn}</Text>) }
+    return (<Text style={styles.textDenNed}>{dn}</Text>)
+  };
+  const Yachejka = ({ el }) => {
+    if (el == "N") { return (<Text style={styles.textN}>{el}</Text>) }
+    if (el == "O") { return (<Text style={styles.textO}>{el}</Text>) }
+    if (el == "R") { return (<Text style={styles.textR}>{el}</Text>) }
 
-			massDate.push([<CellDate _={todayDay == day && (todayMonth + 1) == month} T={dateText} />, <Denned dn={dennedeli} />, <Yachejka el={currentshift} />]);
+    return (<Text style={styles.text}>{el}</Text>)
+  };
 
-			UserDate.startDayOfYear++;
-			UserDate.startD.setDate(UserDate.startD.getDate() + 1);
-		}
-		return massDate;
-	};
+  return (
+    <SafeAreaView style={{ borderWidth: 0, borderColor: "#090", padding: 3, height: "100%" }}>
+      
+        <View style={styles.container}>
+          <View style={{ borderWidth: 1, width: "70%", }}>
+            <FlatList
+              data={createMassDate([shifts[1], shifts[0], shifts[2], shifts[3]])}
+              renderItem={({ item }) => <DateItem item={item} />}
+              keyExtractor={(item, index) => index.toString()} // Уникальный ключ для каждого элемента
+            />
+          </View>
 
-	const CellDate = ({ _, T }) => {
-		return (<Text style={_ ? styles.dateToday : styles.date}>{T}</Text>)
-	};
-	const Yachejka = ({ el }) => {
-		if (el == "N") { return (<Text style={styles.textN}>{el}</Text>) }
-		if (el == "O") { return (<Text style={styles.textO}>{el}</Text>) }
-		if (el == "R") { return (<Text style={styles.textR}>{el}</Text>) }
+          <View style={styles.shift}>
+            <Text>buttons</Text>
+          </View>
 
-		return (<Text style={styles.text}>{el}</Text>)
-	};
-	const Denned = ({ dn }) => {
-		if (dn == "Сб" || dn == "Вс") { return (<Text style={styles.textDenNedV}>{dn}</Text>) }
-
-		return (<Text style={styles.textDenNed}>{dn}</Text>)
-	};
-
-
-	return (
-		<SafeAreaView style={{ borderWidth: 0, borderColor: "#090", padding: 3, height: "100%" }}>
-			<ScrollView>
-				<View style={styles.container}>
-					<View style={{ borderWidth: 1, width: "40%", }}>
-						{createMassDate(shifts[1]).map((element, index) => (
-							<View key={index} style={styles.view1}>
-								{/* <View style={styles.view2}> */}
-								{element[0]}{element[1]}
-								{/* </View> */}
-								{element[2]}
-							</View>
-						))}
-					</View>
-
-					<View style={styles.shift}>
-						<Text>buttons</Text>
-					</View>
-
-				</View>
-			</ScrollView>
-			<View style={{ height: 90, borderWidth: 0, borderColor: "#900", backgroundColor: "#eea" }}></View>
-		</SafeAreaView>
-	);
+        </View>
+      
+      <View style={{ height: 90, borderWidth: 0, borderColor: "#900", backgroundColor: "#eea" }}></View>
+    </SafeAreaView>
+  );
 }
 
-const fS = 14;
-
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		// justifyContent: "space-evenly",
-		// alignItems: "stretch",
-		flexDirection: "row",
-		flexWrap: "wrap",
-		textAlign: "center",
-		paddingBottom: 10,
-		paddingTop: 10,
-		paddingLeft: 10,
-		borderWidth: 1,
-	},
-	view1: {
-		flexDirection: 'row',
-		// overflow: "hidden",
-		// height: 40
-	},
-	// view2: {
-	// 	flexDirection: "row",
-	// 	width: "70%"
-	// },
-	date: {
-		textAlign: "center",
-		textAlignVertical: "center",
-		fontSize: fS,
-		fontWeight: "900",
-
-		borderWidth: 1,
-		borderRadius: 5,
-		margin: 1,
-		color: "#000",
-		backgroundColor: "#fff",
-		borderColor: "#ccc",
-		paddingHorizontal: 8,
-	},
-	dateToday: {
-		textAlign: "center",
-		fontSize: 18,
-		fontWeight: "900",
-		paddingVertical: 3,
-
-		borderWidth: 1,
-		borderRadius: 5,
-		margin: 1,
-		color: "#000",
-		backgroundColor: "#f99",
-		borderColor: "#ccc",
-		paddingHorizontal: 8,
-	},
-	textDenNed: {
-		flexGrow: 1,
-		textAlign: "center",
-		textAlignVertical: 'center',
-		fontSize: fS,
-		fontWeight: "900",
-
-		borderWidth: 1,
-		borderRadius: 5,
-		margin: 1,
-		color: "#000",
-		backgroundColor: "#fff",
-		borderColor: "#ccc",
-		paddingHorizontal: 8,
-	},
-	textDenNedV: {
-		flexGrow: 1,
-		textAlignVertical: 'center',
-		textAlign: "center",
-		fontSize: fS,
-		fontWeight: "900",
-
-		borderWidth: 1,
-		borderRadius: 5,
-		margin: 1,
-		color: "#400",
-		backgroundColor: "#fee",
-		borderColor: "#ccc",
-		paddingHorizontal: 8,
-	},
-	shift: {
-		width: "15%",
-		// height: "100%",
-		borderWidth: 1,
-	},
-	text: {
-		textAlign: "center",
-		textAlignVertical: 'center',
-		fontSize: fS,
-		fontWeight: "900",
-		paddingVertical: 1,
-
-		borderWidth: 1,
-		borderRadius: 5,
-		margin: 1,
-		color: "#000",
-		backgroundColor: "#fff",
-		borderColor: "#ccc",
-		paddingHorizontal: 3,
-
-		// aspectRatio: 1,
-		// alignSelf:"center",
-	},
-	textN: {
-		textAlign: "center",
-		textAlignVertical: 'center',
-		fontSize: fS,
-		fontWeight: "900",
-
-		borderWidth: 1,
-		borderRadius: 5,
-		margin: 1,
-		color: "#FFFFFF",
-		backgroundColor: "#000",
-		borderColor: "#ccc",
-		paddingHorizontal: 8,
-
-		// aspectRatio: 1,
-		// alignSelf:"center",
-	},
-
-	textO: {
-		textAlign: "center",
-		textAlignVertical: 'center',
-		fontSize: fS,
-		fontWeight: "900",
-
-		borderWidth: 1,
-		borderRadius: 5,
-		margin: 1,
-		color: "#ee0",
-		backgroundColor: "#58F",
-		borderColor: "#ccc",
-		paddingHorizontal: 8,
-
-		// aspectRatio: 1,
-		// alignSelf:"center",
-	},
-	textR: {
-		textAlign: "center",
-		textAlignVertical: 'center',
-		fontSize: fS,
-		fontWeight: "900",
-
-		borderWidth: 1,
-		borderRadius: 5,
-		margin: 1,
-		color: "#c22",
-		backgroundColor: "#ffc",
-		borderColor: "#aaa",
-		paddingHorizontal: 8,
-
-		// aspectRatio: 1,
-		// alignSelf:"center",
-	},
+  container: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    textAlign: "center",
+    paddingBottom: 10,
+    paddingTop: 10,
+    paddingLeft: 10,
+    borderWidth: 1,
+  },
+  row: {
+    flexDirection: 'row', // Здесь применяем flexDirection: 'row'
+    overflow: "hidden",
+  },
+  date: {
+    textAlign: "center",
+    textAlignVertical: "center",
+    fontSize: 14,
+    fontWeight: "900",
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 1,
+    color: "#000",
+    backgroundColor: "#fff",
+    borderColor: "#ccc",
+    paddingHorizontal: 8,
+  },
+  dateToday: {
+    textAlign: "center",
+    fontSize: 18,
+    fontWeight: "900",
+    paddingVertical: 3,
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 1,
+    color: "#000",
+    backgroundColor: "#f99",
+    borderColor: "#ccc",
+    paddingHorizontal: 8,
+  },
+  textDenNed: {
+    flexGrow: 1,
+    textAlign: "center",
+    textAlignVertical: 'center',
+    fontSize: 14,
+    fontWeight: "900",
+    paddingHorizontal: 8,
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 1,
+    color: "#000",
+    backgroundColor: "#fff",
+    borderColor: "#ccc",
+  },
+  textDenNedV: {
+    flexGrow: 1,
+    textAlignVertical: 'center',
+    textAlign: "center",
+    fontSize: 14,
+    fontWeight: "900",
+    paddingHorizontal: 8,
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 1,
+    color: "#400",
+    backgroundColor: "#fee",
+    borderColor: "#ccc",
+  },
+  shift: {
+    width: "15%",
+    borderWidth: 1,
+  },
+  text: {
+    textAlign: "center",
+    textAlignVertical: 'center',
+    fontSize: 14,
+    fontWeight: "900",
+    paddingVertical: 1,
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 1,
+    color: "#000",
+    backgroundColor: "#fff",
+    borderColor: "#ccc",
+    paddingHorizontal: 3,
+  },
+  textN: {
+    textAlign: "center",
+    textAlignVertical: 'center',
+    fontSize: 14,
+    fontWeight: "900",
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 1,
+    color: "#FFFFFF",
+    backgroundColor: "#000",
+    borderColor: "#ccc",
+    paddingHorizontal: 8,
+  },
+  textO: {
+    textAlign: "center",
+    textAlignVertical: 'center',
+    fontSize: 14,
+    fontWeight: "900",
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 1,
+    color: "#ee0",
+    backgroundColor: "#58F",
+    borderColor: "#ccc",
+    paddingHorizontal: 8,
+  },
+  textR: {
+    textAlign: "center",
+    textAlignVertical: 'center',
+    fontSize: 14,
+    fontWeight: "900",
+    borderWidth: 1,
+    borderRadius: 5,
+    margin: 1,
+    color: "#c22",
+    backgroundColor: "#ffc",
+    borderColor: "#aaa",
+    paddingHorizontal: 8,
+  },
 });
